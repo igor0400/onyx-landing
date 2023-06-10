b2e816829da6d220697ccd1d5b85909d660ec283
igor0400 <ulanov.igor0400@gmail.com>
2023-06-10 07:04:35 +0300

    made scroll anim

--- src/widgets/Header/components/Navbar.tsx
+++ src/widgets/Header/components/Navbar.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
 import React, { FC } from 'react';
 import { navs } from '../configs/navs';
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 interface Props {
   className?: string;
@@ -11,9 +11,26 @@ const Navbar: FC<Props> = ({ className }) => {
     <nav className={className}>
       <ul className="flex max-xl:flex-col gap-5 uppercase font-light">
         {navs.map(({ title, link }) => (
-          <Link href={link} key={title}>
-            <li>{title}</li>
-          </Link>
+          <React.Fragment key={title}>
+            {link === '/' ? (
+              <div
+                onClick={() => scroll.scrollToTop()}
+                className="cursor-pointer"
+              >
+                <li>{title}</li>
+              </div>
+            ) : (
+              <Link
+                to={link.replace('/#', '')}
+                spy={true}
+                smooth={true}
+                duration={500}
+                className="cursor-pointer"
+              >
+                <li>{title}</li>
+              </Link>
+            )}
+          </React.Fragment>
         ))}
       </ul>
     </nav>
